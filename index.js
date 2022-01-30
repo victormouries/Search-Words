b211eb80644a52213e3dc7483838df87147f781c
victormouries <61155502+victormouries@users.noreply.github.com>
2022-01-30 11:24:30 -0500

    emptyResult

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
-//letter = prompt("Please enter a letter", "u");
-
 
 function findWords(searchString) {
   console.clear();
-  console.log("your search for words with " + searchString + " = ");
+  console.log("Searching for words matching " + searchString);
   let words_found = [];
 
   word_list.forEach(function (word) {
@@ -14,22 +12,3 @@ function findWords(searchString) {
   });
   return words_found;
 }
-
-let searchString = "...ce";
-let words_found = findWords(searchString);
-let result_div = document.getElementById("result");
-
-if (words_found.length > 0) {
-  words_found.forEach(function (word) {
-    let wordDiv = document.createElement("div");
-    wordDiv.className = "word";
-    wordDiv.textContent = word;
-
-    // add the div to the result div
-    result_div.appendChild(wordDiv);
-
-  });
-} else {
-}
-
-//testing
