b1b211e4af695816c364a29d385a6352555897fb
victormouries <61155502+victormouries@users.noreply.github.com>
2022-01-30 10:03:54 -0500

    Changed

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const word_list = ["adieu", "juice", "quant", "music", "voice", "ratch"];
 
-let letter = ".u..e"; // examples = i.e matches juice
 //letter = prompt("Please enter a letter", "u");
-console.clear();
-console.log("your search for words with " + letter + " = ");
-let words_found = 0;
-
-word_list.forEach(function (word) {
-  if (word.match(letter)) {
-    console.log(word);
-    words_found++;
-  }
-});
-if (words_found == 0) {
-  console.log("No Words Found");
-}
 
 function findWords (searchString) {
   alert("in the javascript we searched for " + searchString)
+  console.clear();
+  console.log("your search for words with " + searchString + " = ");
+  let words_found = 0;
+  
+  word_list.forEach(function (word) {
+    if (word.match(searchString)) {
+      console.log(word);
+      words_found++;
+    }
+  });
+  if (words_found == 0) {
+    console.log("No Words Found");
+  }
+
 }
