3244335eb4f4709605837dd75cb1f4d7d7f08b45
victormouries <61155502+victormouries@users.noreply.github.com>
2022-01-30 10:44:46 -0500

    Added

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ const word_list = ["adieu", "juice", "quant", "music", "voice", "ratch"];
 
 //letter = prompt("Please enter a letter", "u");
 
-function findWords (searchString) {
-  alert("in the javascript we searched for " + searchString)
+function findWords(searchString) {
   console.clear();
   console.log("your search for words with " + searchString + " = ");
-  let words_found = 0;
-  
+  let words_found = [];
+
   word_list.forEach(function (word) {
     if (word.match(searchString)) {
-      console.log(word);
-      words_found++;
+      words_found.push(word);
     }
   });
-  if (words_found == 0) {
-    console.log("No Words Found");
-  }
+  return words_found;
+}
 
+let searchString = "...ce";
+let words_found = findWords(searchString);
+let result_div = document.getElementById("result");
+
+if (words_found.length > 0) {
+  words_found.forEach(function (word) {
+    let wordDiv = document.createElement("div");
+    wordDiv.className = "word";
+    wordDiv.textContent = word;
+
+    // add the div to the result div
+    result_div.appendChild(wordDiv);
+  });
+} else {
 }
